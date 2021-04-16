34f2a3a6122f937c15b004a6e49f70a77fc71b90
Khurram Saeed <kssuper007@gmail.com>
2021-04-16 11:36:49 +0500

    Food Delivery App UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,241 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+// building a TodoApp list app flutter
 
-void main() => runApp(const MyApp());
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterninjaapp/bloc/cartListBloc.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp();
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return BlocProvider(
+      blocs: [Bloc((i) => CartListBloc())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'TodoApp',
+        home: Home(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class Kitten {
-  //constructor
-  const Kitten({this.name, this.description, this.age, this.imageUrl});
-
-  final String name;
-  final String description;
-  final int age;
-  final String imageUrl;
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: ListView(
+            children: <Widget>[FirstHalf()],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-final String server =
-    defaultTargetPlatform == TargetPlatform.android ? '10.0.2.2' : 'localhost';
+class FirstHalf extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          CustomAppBar(),
+          SizedBox(
+            height: 30,
+          ),
+          title(),
+          SizedBox(
+            height: 30,
+          ),
+          searchBar(),
+          SizedBox(
+            height: 30,
+          ),
+          catagories(),
+        ],
+      ),
+    );
+  }
 
-final List<Kitten> _kittens = <Kitten>[
-  Kitten(
-      name: 'Mittens',
-      description: 'this is a tipple coat cat and good pet highly recommend',
-      age: 12,
-      imageUrl:
-          'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficatcare.org%2Fadvice%2Fthinking-of-getting-a-cat%2F&psig=AOvVaw0TXZnMYancWwgDK1imqS6o&ust=1618133363616000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNi-l9qu8-8CFQAAAAAdAAAAABAD'),
-  Kitten(
-    name: 'Fluffy',
-    description: 'this is a tipple coat cat and good pet highly recommend',
-    age: 15,
-    imageUrl:
-        'https://cdn.britannica.com/24/212324-050-076731DA/Persian-cat-sleeping.jpg',
-  ),
-  Kitten(
-    name: 'Babri',
-    description: 'this is a tipple coat cat and good pet highly recommend',
-    age: 20,
-    imageUrl:
-        'https://i.natgeofe.com/n/9135ca87-0115-4a22-8caf-d1bdef97a814/75552.jpg?w=636&h=424',
-  ),
-  Kitten(
-    name: 'Kaloo',
-    description: 'this is a tipple coat cat and good pet highly recommend',
-    age: 10,
-    imageUrl:
-        'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
-  ),
-  Kitten(
-    name: 'Shera',
-    description: 'this is a tipple coat cat and good pet highly recommend',
-    age: 6,
-    imageUrl:
-        'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9',
-  ),
-];
+  Widget catagories() {
+    return Container(
+      height: 185,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          CategoryListItem.CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: 'Burgers',
+            avalibility: 12,
+            selected: true,
+          ),
+          CategoryListItem.CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: 'Burgers',
+            avalibility: 12,
+            selected: false,
+          ),
+        ],
+      ),
+    );
+  }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  Widget _dialogBuilder(BuildContext context, Kitten kitten){
-    return SimpleDialog(
-      children: [
-        Image.network(
-          kitten.imageUrl,
-          fit: BoxFit.fill,
+  Widget title() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          width: 45,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Food',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                )),
+            Text(
+              'Delivery',
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+                fontSize: 30,
+              ),
+            )
+          ],
         )
       ],
     );
   }
 
-  Widget _listItemBuilder(BuildContext context, int index){
-    return GestureDetector(
-      onTap: () => showDialog(context: context,
-          builder: (context) => _dialogBuilder(context, _kittens[index])),
-      child: Container(
-        padding: const EdgeInsets.only(left: 16.0),
-        alignment: Alignment.centerLeft,
-        child: Text(_kittens[index].name,
-        style: Theme.of(context).textTheme.headline,
+  Widget searchBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Icon(
+          Icons.search,
+          color: Colors.black45,
         ),
+        SizedBox(width: 20),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: 'Search',
+                contentPadding: EdgeInsets.symmetric(vertical: 10),
+                hintStyle: TextStyle(
+                  color: Colors.black87,
+                )),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.menu),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.only(right: 30),
+              child: Text('0'),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[800],
+                  borderRadius: BorderRadius.circular(50)),
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+class CategoryListItem extends StatelessWidget {
+  final IconData categoryIcon;
+  final String categoryName;
+  final int avalibility;
+  final bool selected;
+
+  CategoryListItem.CategoryListItem({
+    @required this.categoryIcon,
+    @required this.categoryName,
+    @required this.avalibility,
+    @required this.selected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Available cats'),
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: selected ? Color(0xffeb324) : Colors.white,
+          border: Border.all(
+            color: selected ? Colors.transparent : Colors.grey[200],
+            width: 1.5,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[100],
+              blurRadius: 15,
+              offset: Offset(25, 0),
+              spreadRadius: 5,
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: selected ? Colors.transparent : Colors.grey,
+                  width: 1.5,
+                )),
+            child: Icon(
+              categoryIcon,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          SizedBox(height: 10,),
+          Text(
+            categoryName,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            width: 1.5,
+              height: 15,
+            color: Colors.black26,
+          ),
+          Text(
+            avalibility.toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          )
+        ],
       ),
-      body: ListView.builder(
-        itemCount: _kittens.length,
-        itemExtent: 60.0,
-        itemBuilder: _listItemBuilder,
-      ),
-
     );
   }
 }
